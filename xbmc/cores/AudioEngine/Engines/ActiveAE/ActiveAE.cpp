10e466686d1c94c06d88036a4043a145ea0a0956
Portisch <hugo.portisch@yahoo.de>
2026-08-18 16:51:23 +0200

    Revert "[ActiveAE] reset audio cache only on format change"
    This reverts commit ab8de2108d46f0127366b66da83e5d9ff917f15e.

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -1206,6 +1206,7 @@ void CActiveAE::Configure(AEAudioFormat *desiredFmt)
        !CompareFormat(m_sinkRequestFormat, oldSinkRequestFormat)) ||
       m_currDevice.compare(dev.name) != 0 || m_settings.driver.compare(dev.driver) != 0)
   {
+    CServiceBroker::GetDataCacheCore().ResetAudioCache();
     FlushEngine();
     if (!InitSink())
       return;
@@ -1459,7 +1460,6 @@ void CActiveAE::Configure(AEAudioFormat *desiredFmt)
   // reset gui sounds
   if (!CompareFormat(oldInternalFormat, m_internalFormat))
   {
-    CServiceBroker::GetDataCacheCore().ResetAudioCache();
     if (m_settings.guisoundmode == AE_SOUND_ALWAYS ||
        (m_settings.guisoundmode == AE_SOUND_IDLE && m_streams.empty()) ||
        m_aeGUISoundForce)
